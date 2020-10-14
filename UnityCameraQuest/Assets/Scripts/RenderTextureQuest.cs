47a874e5eb6d5ed40ed30eda3e69e0fae5ac93a0
Maxim Kruk <maximkr@playtika.com>
2020-10-14 18:20:26 +0300

    RenderTextureDone

---
+++ UnityCameraQuest/Assets/Scripts/RenderTextureQuest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderTextureQuest : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private Shader _shader;
+    private Renderer _renderer;
+    private RenderTexture _rTexture;
+
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+        _rTexture = new RenderTexture(1024, 1024, 16, RenderTextureFormat.ARGB32);
+        _rTexture.Create();
+        _camera.targetTexture = _rTexture;
+        _renderer.material = new Material(_shader);
+        _renderer.material.mainTexture = _rTexture;
+    }
+}
